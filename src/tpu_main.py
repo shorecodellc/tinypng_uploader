b72d414125f565387e838b0e798c7b143ca795fc
Kevin Fink <kevin@shorecode.org>
2025-04-05 11:10:57 +0700

    dev

--- src/tpu_main.py
+++ src/tpu_main.py
@@ -15,7 +15,7 @@ filepaths = files.get_files_list()
 output_dir = filepaths[1]
 input_dir = filepaths[2]
 input_thumbs_dir = filepaths[3]
-INPUT_RESIZE_DIR = filepaths[3]
+INPUT_RESIZE_DIR = filepaths[4]
 #log_fp = filepaths[0]
 
 #logger = set_logging('', log_fp)
@@ -23,11 +23,13 @@ INPUT_RESIZE_DIR = filepaths[3]
 tinify.key = os.getenv('TINYPNG_API_KEY')
 
 class TinyPngUploader:
-    def __init__(self, input_dir, thumb_input_dir, resize_input_dir):
+    def __init__(self, input_dir, thumb_input_dir, resize_input_dir, width=None, height=None):
         self.threads = list()
         self.input_dir = input_dir
         self.thumb_input_dir = thumb_input_dir
         self.resize_input_dir = resize_input_dir
+        self.width = width
+        self.height = height
 
     def run(self, thumb_list, convert_list, resize_list):
         for file in input_files:
@@ -39,7 +41,7 @@ class TinyPngUploader:
             self.threads.append(thread)
             thread.start()
         for file in resize_list:
-            thread = threading.Thread(target=lambda: self.resize(os.path.join(self.resize_input_dir, file)))
+            thread = threading.Thread(target=lambda: self.resize(os.path.join(self.resize_input_dir, file), self.width, self.height))
             self.threads.append(thread)
             thread.start()
         for t in self.threads:            
@@ -62,21 +64,21 @@ class TinyPngUploader:
         output_fp = os.path.join(output_dir, os.path.split(img)[-1])
         resized.to_file(output_fp)
         
-    def resize(self, img, width, height):    
+    def resize(self, img, width, height):
         source = tinify.from_file(img)
         source = source.resize(
-            method="thumb",
+            method="fit",
             width=width,
             height=height
         )      
         output_fp = os.path.join(output_dir, os.path.split(img)[-1])
-        resized.to_file(output_fp)    
+        source.to_file(output_fp)    
 
 if __name__ == '__main__':
     input_files = list(os.walk(input_dir))[0][2]
     input_files_for_thumbs =list(os.walk(input_thumbs_dir))[0][2]
     input_files_for_resize =list(os.walk(INPUT_RESIZE_DIR))[0][2]
-    png = TinyPngUploader(input_dir, input_thumbs_dir)
-    png.run(input_files_for_thumbs, input_files)
+    png = TinyPngUploader(input_dir, input_thumbs_dir, INPUT_RESIZE_DIR, width=268, height=183)
+    png.run(input_files_for_thumbs, input_files, input_files_for_resize)
 
 
